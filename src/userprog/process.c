93a7f0ce77437863608d1ad1dfbc286a1b694b70
DaehunGwak <pmon2648@daum.net>
2019-05-14 19:18:55 +0900

    Edit parameter file_name to just binary file. when call load() in start_process(). Add more debug codes.

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -159,7 +159,7 @@ start_process (void *file_name_)
   if_.gs = if_.fs = if_.es = if_.ds = if_.ss = SEL_UDSEG;
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
-  success = load (file_name, &if_.eip, &if_.esp, &f_argv);
+  success = load (f_argv.f_head->next->c_str, &if_.eip, &if_.esp, &f_argv);
 
   /* If load failed, quit. */
   palloc_free_page (file_name);
@@ -320,6 +320,10 @@ load (const char *file_name, void (**eip) (void), void **esp, struct f_token_lis
   off_t file_ofs;
   bool success = false;
   int i;
+
+  //custom: debuggin
+  printf("load():\n");
+  f_token_list_debug(f_argv);
 
   /* Allocate and activate page directory. */
   t->pagedir = pagedir_create ();
